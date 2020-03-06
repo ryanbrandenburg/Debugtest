d91df9910def32ac35a3eba719ba9cb632248ce4
Ryan Brandenburg <rybrande@microsoft.com>
2020-03-06 14:29:14 -0800

    Initial commit test debug

---
+++ Tests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace SomeNamespace
+{
+    public class Test
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            Assert.Equal(1, 1);
+            Assert.Equal(2, 2);
+        }
+    }
+}
